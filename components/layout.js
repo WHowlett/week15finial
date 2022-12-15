a4e60ae05f408a428a5e929e0d2c3dd7e356929b
Wayne Howlett <wmhowlett89@icloud.com>
2022-12-15 02:29:17 -0300

    I hate the LINK sometime

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,9 @@ export default function Layout( { children, home } ) {
       <header>
       <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">WH</a>
+    <Link href="/">
+    <a class="navbar-brand">WH</a>
+    </Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
